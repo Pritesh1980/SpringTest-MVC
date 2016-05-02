599a981b468b4cca75b06696c3881ab41709ba75
Pritesh <me@pritesh.net>
2016-05-02 14:22:26 +0100

    Initial commit. Working basic app, plus static pdf

---
+++ src/main/java/net/pritesh/controller/MinutesController.java
@@ -0,0 +1,28 @@
+package net.pritesh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.pritesh.model.Exercise;
+
+@Controller
+public class MinutesController
+{
+	@RequestMapping(value = "/addMinutes")
+	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise)
+	{
+		System.out.println("exercise: " + exercise.getMinutes());
+		
+		return "addMinutes";
+		//return "forward:addMoreMinutes.html";
+	}
+
+//	@RequestMapping(value = "/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise)
+//	{
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		return "addMinutes";
+//	}
+
+}
---
+++ src/main/java/net/pritesh/model/Exercise.java
@@ -0,0 +1,16 @@
+package net.pritesh.model;
+
+public class Exercise
+{
+	private int minutes;
+
+	public final int getMinutes()
+	{
+		return minutes;
+	}
+
+	public final void setMinutes(int minutes)
+	{
+		this.minutes = minutes;
+	}
+}
